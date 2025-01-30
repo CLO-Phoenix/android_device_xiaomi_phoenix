913a2afdd0768c2a5a8e06ed34487a55c5b6a81a
Naruto-242 <Naruto242@proton.me>
2025-01-30 17:33:34 +0530

    phoenix: Enable elfchecks for libgrallocutils

--- extract-files.py
+++ extract-files.py
@@ -9,7 +9,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -18,7 +17,7 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
-namespace_imports = [
+namespace_imports = [    
     'hardware/xiaomi',
     'vendor/qcom/common/vendor/adreno-5xx',
     'vendor/qcom/common/vendor/adreno-r',
@@ -32,9 +31,7 @@ namespace_imports = [
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    'libgrallocutils': lib_fixup_remove,
 }
-
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
